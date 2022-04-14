9cb02358722133d8fcb69c9d55d9e652450b8db7
Takashi Yanagisawa (N3 <v-tayana@microsoft.com>
2022-04-14 19:42:20 +0800

    update

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -26,12 +26,12 @@ namespace inventory.Pages
             List<Book> books=null;
             
             // UNCOMMENT AFTER SETTING THE CONNECTION STRING
-            // try  {
-            //     books=_context.Books.ToList();
-            // }
-            // catch (Exception ex)  {
-            //     ViewData["Error"]=ex.Message;
-            // }
+            try  {
+                books=_context.Books.ToList();
+            }
+            catch (Exception ex)  {
+                ViewData["Error"]=ex.Message;
+            }
             ViewData["books"]=books;
 
         }
